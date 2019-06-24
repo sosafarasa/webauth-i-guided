e7a7dde36ff2d667ee0dffc6ec3742c4842baf62
Agustina Sosa <agustinasf85@gmail.com>
2019-06-24 13:48:35 -0400

    guided project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -17,7 +18,8 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
-
+  const hash = bcrypt.hashSync(user.password, 8) // # means the amount of times that the password will be re-hashed
+  user.password = hash;
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,7 +35,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,13 +46,39 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next){
+  // To keep this as an get endpoint we can't send any data in the body.
+  // We do have access to headers
+  // The middleware is going to check if the username and password are coming from the headers
+  // if there is no username or password on the header send a 400 and a rejection message
+  //if there is user and pass then check for the user to exist and the pass to macth the existing user
+  const { username, password } = req.headers;
+
+  if( username && password){
+    Users.findBy({ username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      next()
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  });
+  } else {
+    res.status(400).json({ message: 'No credentials provided'})
+  }
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
